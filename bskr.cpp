e423663c6b7dbd6b6a5c1d1154fa9f5d5ad27a45
ZeroRevolution <0josegabrielmatos0@gmail.com>
2022-11-01 08:55:59 -0300

    modified:   bskr 	renamed:    bskr.c -> bskr.cpp

--- bskr.cpp
+++ bskr.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
     int a = strtol(argv[1], NULL, 10);
     int b = strtol(argv[2], NULL, 10);
