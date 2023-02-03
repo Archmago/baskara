99e0b6690735c7d6a90706e11256a63db51124ed
ZeroRevolution <0josegabrielmatos0@gmail.com>
2023-02-03 17:27:24 -0300

    new file:   bskr 	modified:   bskr.c

--- bskr.c
+++ bskr.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
-    // Variables / logic
+    // Variables / Calc
     int a = strtol(argv[1], NULL, 10);
     int b = strtol(argv[2], NULL, 10);
     int c = strtol(argv[3], NULL, 10);
