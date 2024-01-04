d55b620f632792e87272dae7dcd58738b143c129
José Gabriel <66148656+Archmago@users.noreply.github.com>
2024-01-04 18:23:05 -0300

    Update bskr.c

--- bskr.c
+++ bskr.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
-    // Variables / Calc
+    
     int a = strtol(argv[1], NULL, 10);
     int b = strtol(argv[2], NULL, 10);
     int c = strtol(argv[3], NULL, 10);
@@ -11,11 +11,12 @@ int main(int argc, char* argv[]) {
     float result1 = (-b + sqrt(delta)) / (2 * a);
     float result2 = (-b - sqrt(delta)) / (2 * a);
 
-    // Output
     if(delta >= 0) {
         printf("A: %i\nB: %i\nC: %i\n\nDelta: %i\n\nResultado 1: %.2f\nResultado 2: %.2f\n", a, b ,c, delta ,result1, result2);
     } else {
         printf("A: %i\nB: %i\nC: %i\n\nDelta: %i\n\nSem resultados possiveis, delta negativo!\n", a, b ,c, delta);
     }
+    
     return EXIT_SUCCESS;
+    
 }
